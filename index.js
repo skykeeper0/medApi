1f872e30320b837f75abe27e3c62396c2a1763ab
skykeeper0 <trungdinh0429@gmail.com>
2017-05-04 20:37:31 -0500

    add CORS and listen method

--- index.js
+++ index.js
@@ -2,4 +2,23 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
+app.use(bodyParser.json());
+
+// CORS.
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    console.log('OPTIONS');
+    return next();
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`)
+})
