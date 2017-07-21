bc7538b043be48bff122a1e36e3ca3b10fb8dd43
skykeeper0 <trungdinh0429@gmail.com>
2017-07-20 20:00:22 -0700

    clean out unneeded comment

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,3 @@
-// const mongoose = require('mongoose')
-// const conf = require('../shared/config')();
-// // const sequelize = new Sequelize(`postgres://${conf.username}@${conf.host}:${conf.port}/${conf.database}`)
-// var db;
-// const url = `mongodb://@localhost:27017/med`;
-// mongoose.connect(url, (err, database) => {
-//   db = database;
-// })
-// var connection = mongoose.connection;
-// connection.on('error', console.error.bind(console, 'connection error:'));
-// connection.once('open', function() {
-//   console.log('database connected')
-// });
-
 const MongoClient = require('mongodb').MongoClient;
 const Server = require('mongodb').Server;
 const conf = require('../shared/config')();
